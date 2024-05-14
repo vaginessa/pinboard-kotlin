5867d084b03e4be1cadcf5f1abe53d5b3bfb09a4
Filipe Belatti <fibelatti@gmail.com>
2024-05-15 00:16:21 +0100

    Update image and configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -81,9 +81,9 @@ subprojects {
 
                 managedDevices {
                     devices {
-                        maybeCreate<com.android.build.api.dsl.ManagedVirtualDevice>("pixel2api30").apply {
-                            device = "Pixel 2"
-                            apiLevel = 30
+                        maybeCreate<com.android.build.api.dsl.ManagedVirtualDevice>("pixel6api33").apply {
+                            device = "Pixel 6"
+                            apiLevel = 33
                             systemImageSource = "aosp-atd"
                         }
                     }
